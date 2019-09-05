83e9188deb940606775df82448f8ed83e62b2d64
Ariel Barajas <ari.eldisenador@gmail.com>
2019-09-05 13:39:57 -0500

    Se actualiza a la versión correcta y funcional, se agrega carpeta node_modules

--- public/app.js
+++ public/app.js
@@ -11,8 +11,8 @@
                     .done(function (data) {
                         if (!data.error) {
                             console.log(data)
-                            $('#ciudad').append(self.renderSelect(data.ciudades))
-                            $('#tipo').append(self.renderSelect(data.tipos))
+                            $("#ciudad").append(self.renderSelect(data.ciudades))
+                            $("#tipo").append(self.renderSelect(data.tipos))
                             $("#ciudad").material_select()
                             $("#tipo").material_select()
                         }
@@ -26,12 +26,11 @@
                     from: 1000,
                     to: 20000,
                     prefix: "$"
-                })
-                //realiza la consulta de todos los datos
-                $('#buscar').click(function () {
+                });
+                $("#buscar").click(function () {
                     if ($("#checkPersonalizada")[0].checked) {
                         var valores = $("#rangoPrecio").val()
-                        valores = valores.split("")
+                        valores = valores.split(";")
                         var urls = `http://localhost:8080/ciudad/${$("#ciudad").val()}/tipo/${$("#tipo").val()}/desde/${valores[0]}/hasta/${valores[1]}`
                     } else {
                         var urls = "http://localhost:8080/search"
@@ -43,66 +42,65 @@
                         })
                         .done(function (data) {
                             if (!data.error) {
-                                console.log(data)
-                                $('.lista').html(self.renderCard(data.datos))
+                                $(".lista").html(self.renderCard(data.datos))
                             }
-                        })
-                })
-                self.setSearch()
+                        });
+                });
+                self.setSearch();
 
             },
             setSearch: function () {
-                let busqueda = $('#checkPersonalizada')
-                busqueda.on('change', (e) => {
+                let busqueda = $("#checkPersonalizada")
+                busqueda.on("change", (e) => {
                     if (this.customSearch == false) {
-                        this.customSearch = true
+                        this.customSearch = true;
                     } else {
-                        this.customSearch = false
+                        this.customSearch = false;
                     }
-                    $('#personalizada').toggleClass('invisible')
+                    $("#personalizada").toggleClass("invisible")
                 })
             },
             renderSelect: function (data) {
-                var html = ''
+                var html = ""
                 data.forEach(function (key, idx) {
                     html += `<option value="${key}">${key}</option>`
-                })
+                });
                 return html
             },
             renderCard: function (obj) {
-                var html = ''
+                var html = ""
                 obj.forEach(function (key, idx) {
                     html += `<div class="card horizontal">
-                              <div class="card-image">
-                                  <img src="http://localhost:8080/img/home.jpg">
-                              </div>
-                              <div class="card-stacked">
-                                  <div class="card-content">
-                                      <div>
-                                          <p><strong>Direccion: </strong>${ key.Direccion }</p>
-                                      </div>
-                                      <div>
-                                          <p><strong>Ciudad: </strong>${ key.Ciudad }</p>
-                                      </div>
-                                      <div>
-                                          <p><strong>Telefono: </strong>${ key.Telefono }</p>
-                                      </div>
-                                      <div>
-                                          <p><strong>Código postal: </strong>${ key.Codigo_Postal }</p>
-                                      </div>
-                                      <div>
-                                          <p><strong>Precio: </strong>${ key.Precio }</p>
-                                      </div>
-                                      <div>
-                                          <p><strong>Tipo: </strong>${ key.Tipo }</p>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>`
+                                <div class="card-image">
+                                    <img src="http://localhost:8080/img/home.jpg">
+                                </div>
+                                <div class="card-stacked">
+                                    <div class="card-content">
+                                        <div>
+                                            <p><strong>Direccion: </strong>${ key.Direccion }</p>
+                                        </div>
+                                        <div>
+                                            <p><strong>Ciudad: </strong>${ key.Ciudad }</p>
+                                        </div>
+                                        <div>
+                                            <p><strong>Telefono: </strong>${ key.Telefono }</p>
+                                        </div>
+                                        <div>
+                                            <p><strong>Código postal: </strong>${ key.Codigo_Postal }</p>
+                                        </div>
+                                        <div>
+                                            <p><strong>Precio: </strong>${ key.Precio }</p>
+                                        </div>
+                                        <div>
+                                            <p><strong>Tipo: </strong>${ key.Tipo }</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>`
                 })
-                return html
+                return html;
             }
-        }
+        };
     })()
     search.Init()
-})(document, window, undefined, jQuery)
+})(document, window, undefined, jQuery);
